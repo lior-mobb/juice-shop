8d1dbc44fbb22486b0e2cb0d9b1dd84227d3001d
Mobb autofixer <git@mobb.ai>
2026-06-02 12:11:34 +0000

    mobb fix commit: c21b8128-fe61-4f1c-9e09-1f56957c3210

--- routes/captcha.ts
+++ routes/captcha.ts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 
+import * as crypto from 'crypto'
 import { type Request, type Response, type NextFunction } from 'express'
 import { type Captcha } from '../data/types'
 import { CaptchaModel } from '../models/captcha'
@@ -17,7 +18,7 @@ function captchas () {
     const thirdTerm = Math.floor((Math.random() * 10) + 1)
 
     const firstOperator = operators[Math.floor((Math.random() * 3))]
-    const secondOperator = operators[Math.floor((Math.random() * 3))]
+    const secondOperator = operators[Math.floor(((crypto.getRandomValues(new Uint32Array(1))[0] / 2 ** 32) * 3))]
 
     const expression = firstTerm.toString() + firstOperator + secondTerm.toString() + secondOperator + thirdTerm.toString()
     const answer = eval(expression).toString() // eslint-disable-line no-eval
