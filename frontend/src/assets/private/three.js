4b1223db8bf868d0dcb1c74a8bc0a586c44d0e98
Mobb autofixer <git@mobb.ai>
2026-06-02 12:11:43 +0000

    mobb fix commit: b40a65f4-3fe6-4d62-a468-ec892f9ffbbb

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -34849,7 +34849,7 @@ THREE.PolyhedronGeometry = function ( vertices, indices, radius, detail ) {
 		var uvs = this.faceVertexUvs[ 0 ][ i ];
 
 		var x0 = uvs[ 0 ].x;
-		var x1 = uvs[ 1 ].x;
+		const x1 = uvs[ 1 ].x;
 		var x2 = uvs[ 2 ].x;
 
 		var max = Math.max( x0, Math.max( x1, x2 ) );
