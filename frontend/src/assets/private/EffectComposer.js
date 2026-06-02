2e1e49d01bf69ff04aa6a9a7d7def1930566807f
Mobb autofixer <git@mobb.ai>
2026-06-02 12:11:42 +0000

    mobb fix commit: 8eb85376-150d-4c3f-9aa9-602f0b65e4ed

--- frontend/src/assets/private/EffectComposer.js
+++ frontend/src/assets/private/EffectComposer.js
@@ -10,7 +10,7 @@ THREE.EffectComposer = function ( renderer, renderTarget ) {
 
 		var width = window.innerWidth || 1;
 		var height = window.innerHeight || 1;
-		var parameters = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat, stencilBuffer: false };
+		const parameters = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat, stencilBuffer: false };
 
 		renderTarget = new THREE.WebGLRenderTarget( width, height, parameters );
 
