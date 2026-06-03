2cbf00fffd626ffe2353d61769c5f7c3ba700abe
Mobb autofixer <git@mobb.ai>
2026-06-03 22:28:55 +0000

    mobb fix commit: 48731559-3ad0-453c-8ec1-cf429c3774e1

--- data/static/codefixes/loginBenderChallenge_4.ts
+++ data/static/codefixes/loginBenderChallenge_4.ts
@@ -14,7 +14,7 @@ module.exports = function login () {
   }
 
   return (req: Request, res: Response, next: NextFunction) => {
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: models.User, plain: false })
+    models.sequelize.query('SELECT * FROM Users WHERE email = ? AND password = ? AND deletedAt IS NULL', { replacements: [req.body.email || '', security.hash(req.body.password || '')], model: models.User, plain: false })
       .then((authenticatedUser) => {
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
