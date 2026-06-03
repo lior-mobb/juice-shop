99973e55851294438052f00049c3b3b0a75ba1d9
Mobb autofixer <git@mobb.ai>
2026-06-03 22:21:22 +0000

    mobb fix commit: 2ef06625-22f4-46c4-9e8a-d383c450c057

--- routes/profileImageUrlUpload.ts
+++ routes/profileImageUrlUpload.ts
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: MIT
  */
 
+import * as path from 'node:path'
 import fs = require('fs')
 import { type Request, type Response, type NextFunction } from 'express'
 import logger from '../lib/logger'
@@ -27,7 +28,8 @@ module.exports = function profileImageUrlUpload () {
           })
           .on('response', function (res: Response) {
             if (res.statusCode === 200) {
-              const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? url.split('.').slice(-1)[0].toLowerCase() : 'jpg'
+              const safeInput = path.basename(String(url.split('.').slice(-1)[0] || '').replace('\0', '').replace(/^(\.\.(\/|\\$))+/, ''))
+              const ext = ['jpg', 'jpeg', 'png', 'svg', 'gif'].includes(url.split('.').slice(-1)[0].toLowerCase()) ? safeInput.toLowerCase() : 'jpg'
               imageRequest.pipe(fs.createWriteStream(`frontend/dist/frontend/assets/public/images/uploads/${loggedInUser.data.id}.${ext}`))
               UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: `/assets/public/images/uploads/${loggedInUser.data.id}.${ext}` }) }).catch((error: Error) => { next(error) })
             } else UserModel.findByPk(loggedInUser.data.id).then(async (user: UserModel | null) => { return await user?.update({ profileImage: url }) }).catch((error: Error) => { next(error) })
