8f8b4a6ca98bd638ff22c678031f242a42f83b5d
Mobb autofixer <git@mobb.ai>
2026-06-05 17:41:21 +0000

    mobb fix commit: 68e38c4e-6b5c-4503-bdc3-54c02b8cc909

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -28480,7 +28480,7 @@ THREE.ImageUtils = {
 		var size = width * height;
 		var data = new Uint8Array( 3 * size );
 
-		var r = Math.floor( color.r * 255 );
+		const r = Math.floor( color.r * 255 );
 		var g = Math.floor( color.g * 255 );
 		var b = Math.floor( color.b * 255 );
 
