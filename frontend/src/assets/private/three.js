11f0dbcfb8149abcc5deed6229fc3136097c5548
Mobb autofixer <git@mobb.ai>
2026-06-05 17:54:48 +0000

    mobb fix commit: 85ff8a0b-1e16-4412-809b-41e680e61f8d

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -722,7 +722,7 @@ THREE.Quaternion.prototype = {
 		//	content/SpinCalc.m
 
 		var c1 = Math.cos( euler._x / 2 );
-		var c2 = Math.cos( euler._y / 2 );
+		const c2 = Math.cos( euler._y / 2 );
 		var c3 = Math.cos( euler._z / 2 );
 		var s1 = Math.sin( euler._x / 2 );
 		var s2 = Math.sin( euler._y / 2 );
