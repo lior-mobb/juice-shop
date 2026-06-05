7b69e07cdf08af9b5bdfea7766e307086f67bc71
Mobb autofixer <git@mobb.ai>
2026-06-05 17:40:38 +0000

    mobb fix commit: 7d77552b-56a6-4aa5-b0e2-97a3533a3624

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -7747,7 +7747,7 @@ THREE.Object3D.prototype = {
 
 	translateY: function () {
 
-		var v1 = new THREE.Vector3( 0, 1, 0 );
+		const v1 = new THREE.Vector3( 0, 1, 0 );
 
 		return function ( distance ) {
 
