4765e38c4d35af71367ac8fdec659f989a6b0f2b
Mobb autofixer <git@mobb.ai>
2026-06-05 16:40:42 +0000

    mobb fix commit: 168b2693-ec32-4ebd-a917-2f6b3ab17b23

--- routes/dataErasure.ts
+++ routes/dataErasure.ts
@@ -66,7 +66,8 @@ router.post('/', async (req: Request<Record<string, unknown>, Record<string, unk
 
     res.clearCookie('token')
     if (req.body.layout) {
-      const filePath: string = path.resolve(req.body.layout).toLowerCase()
+      const safeInput = path.basename(String(req.body.layout || '').replace('\0', '').replace(/^(\.\.(\/|\\$))+/, ''))
+      const filePath: string = path.resolve(safeInput).toLowerCase()
       const isForbiddenFile: boolean = (filePath.includes('ftp') || filePath.includes('ctf.key') || filePath.includes('encryptionkeys'))
       if (!isForbiddenFile) {
         res.render('dataErasureResult', {
