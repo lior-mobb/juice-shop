8a843c7df1868f801a5d4c2ee126d79d38441a18
Mobb autofixer <git@mobb.ai>
2026-06-05 17:41:13 +0000

    mobb fix commit: ec13024d-f7dd-4f29-846a-b365b09fe81c

--- test/api/metricsApiSpec.ts
+++ test/api/metricsApiSpec.ts
@@ -14,7 +14,7 @@ describe('/metrics', () => {
     return frisby.get(API_URL)
       .expect('status', 200)
       .expect('header', 'content-type', /text\/plain/)
-      .expect('bodyContains', /^.*_version_info{version="[0-9]+.[0-9]+.[0-9]+(-SNAPSHOT)?",major="[0-9]+",minor="[0-9]+",patch="[0-9]+",app=".*"} 1$/gm)
+      .expect('bodyContains', /^[^\n]*_version_info{version="[0-9]+.[0-9]+.[0-9]+(-SNAPSHOT)?",major="[0-9]+",minor="[0-9]+",patch="[0-9]+",app="[^\n]*"} 1$/gm)
       .expect('bodyContains', /^.*_challenges_solved{difficulty="[1-6]",category=".*",app=".*"} [0-9]*$/gm)
       .expect('bodyContains', /^.*_challenges_total{difficulty="[1-6]",category=".*",app=".*"} [0-9]*$/gm)
       .expect('bodyContains', /^.*_cheat_score{app=".*"} [0-9.]*$/gm)
