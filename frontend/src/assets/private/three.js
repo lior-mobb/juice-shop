8710a414b1e2a2f56c9a1af94d7d6fe0a0e7961c
Mobb autofixer <git@mobb.ai>
2026-06-05 17:55:35 +0000

    mobb fix commit: b43b55c1-3e71-48a0-a977-c147e3cca38f

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -13907,7 +13907,7 @@ THREE.SceneLoader.prototype = {
 
 				counter_textures += textureJSON.url.length;
 
-				for( var n = 0; n < textureJSON.url.length; n ++ ) {
+				for( let n = 0; n < textureJSON.url.length; n ++ ) {
 
 					scope.onLoadStart();
 
