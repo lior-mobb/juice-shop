8fc7a1e91c326292caf165751e3575b6ec2e73f3
Mobb autofixer <git@mobb.ai>
2026-06-05 17:39:47 +0000

    mobb fix commit: 7ed50bfa-3a06-40af-9602-1cc8c336b0a1

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -5217,7 +5217,7 @@ THREE.Matrix4.prototype = {
 		var y = 2 * near / ( top - bottom );
 
 		var a = ( right + left ) / ( right - left );
-		var b = ( top + bottom ) / ( top - bottom );
+		const b = ( top + bottom ) / ( top - bottom );
 		var c = - ( far + near ) / ( far - near );
 		var d = - 2 * far * near / ( far - near );
 
