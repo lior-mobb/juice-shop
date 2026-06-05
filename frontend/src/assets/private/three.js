4717bf59989d82b7c8f65b6ce462bbe106b58e72
Mobb autofixer <git@mobb.ai>
2026-06-05 17:39:40 +0000

    mobb fix commit: 76210362-7e31-4d5d-beb0-21616b159c00

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -36227,7 +36227,7 @@ THREE.VertexNormalsHelper.prototype.update = ( function ( object ) {
 
 		var worldMatrix = this.object.matrixWorld;
 
-		var idx = 0;
+		let idx = 0;
 
 		for ( var i = 0, l = faces.length; i < l; i ++ ) {
 
