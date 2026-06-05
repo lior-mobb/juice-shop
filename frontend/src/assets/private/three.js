cce3199980d63825c67019aa796cc2a9f10f6395
Mobb autofixer <git@mobb.ai>
2026-06-05 17:56:18 +0000

    mobb fix commit: 9019672a-3049-4ee9-ae74-75eff954d60a

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -10023,7 +10023,7 @@ THREE.Geometry.prototype = {
 
 		for ( var i = 0, il = this.faces.length; i < il; i ++ ) {
 
-			var face = this.faces[ i ];
+			const face = this.faces[ i ];
 			face.normal.applyMatrix3( normalMatrix ).normalize();
 
 			for ( var j = 0, jl = face.vertexNormals.length; j < jl; j ++ ) {
