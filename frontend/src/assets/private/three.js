f8920cf21d606a28f381a29dcf08719bde7aa3fa
Mobb autofixer <git@mobb.ai>
2026-06-05 17:55:00 +0000

    mobb fix commit: e47f13ff-58f8-4456-92fb-5c8942c572d8

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -36393,7 +36393,7 @@ THREE.WireframeHelper = function ( object, hex ) {
 				edge[ 1 ] = face[ keys[ ( j + 1 ) % 3 ] ];
 				edge.sort( sortFunction );
 
-				var key = edge.toString();
+				const key = edge.toString();
 
 				if ( hash[ key ] === undefined ) {
 
