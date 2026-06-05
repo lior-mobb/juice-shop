803552c9c979c308599736492efff06e57a87886
Mobb autofixer <git@mobb.ai>
2026-06-05 17:42:02 +0000

    mobb fix commit: e58bb770-1486-4a49-9148-3e37d1d5c66d

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -3039,7 +3039,7 @@ THREE.Euler.prototype = {
 		// assumes the upper 3x3 of m is a pure rotation matrix (i.e, unscaled)
 
 		var te = m.elements;
-		var m11 = te[0], m12 = te[4], m13 = te[8];
+		let m11 = te[0], m12 = te[4], m13 = te[8];
 		var m21 = te[1], m22 = te[5], m23 = te[9];
 		var m31 = te[2], m32 = te[6], m33 = te[10];
 
