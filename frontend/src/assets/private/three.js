fcdbfd6d653bf2b5f8d1722d7126c5ed95bb4316
Mobb autofixer <git@mobb.ai>
2026-06-05 17:54:56 +0000

    mobb fix commit: 02d24f79-a699-4685-bffa-81efa50a24e5

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -12838,7 +12838,7 @@ THREE.ObjectLoader.prototype = {
 			for ( var i = 0, l = json.length; i < l; i ++ ) {
 
 				var data = json[ i ];
-				var material = loader.parse( data );
+				const material = loader.parse( data );
 
 				material.uuid = data.uuid;
 
