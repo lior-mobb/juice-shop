9a4c3995ff0ac08243b982782ba5ca0cd47a625b
Mobb autofixer <git@mobb.ai>
2026-06-05 17:55:25 +0000

    mobb fix commit: a0ef36cb-bb50-406a-9a9f-c128cc319c07

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -29810,7 +29810,7 @@ THREE.Path.prototype.quadraticCurveTo = function( aCPx, aCPy, aX, aY ) {
 	var lastargs = this.actions[ this.actions.length - 1 ].args;
 
 	var x0 = lastargs[ lastargs.length - 2 ];
-	var y0 = lastargs[ lastargs.length - 1 ];
+	const y0 = lastargs[ lastargs.length - 1 ];
 
 	var curve = new THREE.QuadraticBezierCurve( new THREE.Vector2( x0, y0 ),
 												new THREE.Vector2( aCPx, aCPy ),
