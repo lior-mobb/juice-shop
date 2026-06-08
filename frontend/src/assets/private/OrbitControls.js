72215c7511dd8058fd68e8bc3b6066ecb4969cfb
Mobb autofixer <git@mobb.ai>
2026-06-08 15:48:39 +0000

    mobb fix commit: 0c195bae-d17a-46ae-9cd3-8c8a17f80e81

--- frontend/src/assets/private/OrbitControls.js
+++ frontend/src/assets/private/OrbitControls.js
@@ -75,7 +75,7 @@ THREE.OrbitControls = function ( object, domElement ) {
     var EPS = 0.000001;
 
     var rotateStart = new THREE.Vector2();
-    var rotateEnd = new THREE.Vector2();
+    const rotateEnd = new THREE.Vector2();
     var rotateDelta = new THREE.Vector2();
 
     var panStart = new THREE.Vector2();
