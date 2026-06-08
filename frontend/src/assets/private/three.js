9c4917857f1c39a02067e650904d271f44328b18
Mobb autofixer <git@mobb.ai>
2026-06-08 13:41:47 +0000

    mobb fix commit: 69f38458-8296-4545-9795-7ff891332eab

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -4208,7 +4208,7 @@ THREE.Matrix3.prototype = {
 
 		if ( det === 0 ) {
 
-			var msg = "Matrix3.getInverse(): can't invert matrix, determinant is 0";
+			const msg = "Matrix3.getInverse(): can't invert matrix, determinant is 0";
 
 			if ( throwOnInvertible || false ) {
 
