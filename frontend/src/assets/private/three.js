6ae4914df061a0bc77d226a83006f12e05acce17
Mobb autofixer <git@mobb.ai>
2026-06-08 15:49:43 +0000

    mobb fix commit: aff5878b-0f3b-4400-856f-c69c622d1eba

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -34063,7 +34063,7 @@ THREE.PlaneGeometry = function ( width, height, widthSegments, heightSegments ) 
 
 			var uva = new THREE.Vector2( ix / gridX, 1 - iz / gridZ );
 			var uvb = new THREE.Vector2( ix / gridX, 1 - ( iz + 1 ) / gridZ );
-			var uvc = new THREE.Vector2( ( ix + 1 ) / gridX, 1 - ( iz + 1 ) / gridZ );
+			const uvc = new THREE.Vector2( ( ix + 1 ) / gridX, 1 - ( iz + 1 ) / gridZ );
 			var uvd = new THREE.Vector2( ( ix + 1 ) / gridX, 1 - iz / gridZ );
 
 			var face = new THREE.Face3( a, b, d );
