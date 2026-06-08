3b7a04e76ff9641c605fd6273e43b9e48ab87566
Mobb autofixer <git@mobb.ai>
2026-06-08 13:40:13 +0000

    mobb fix commit: 66a0cf2b-d9c0-4cfb-b786-e4ae9348b709

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -31827,7 +31827,7 @@ THREE.Animation.prototype.update = (function(){
 		for ( var h = 0, hl = this.hierarchy.length; h < hl; h ++ ) {
 
 			var object = this.hierarchy[ h ];
-			var animationCache = object.animationCache[this.data.name];
+			const animationCache = object.animationCache[this.data.name];
 
 			// loop through pos/rot/scl
 
