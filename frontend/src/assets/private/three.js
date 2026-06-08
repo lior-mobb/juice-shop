d113e3bfda5efdde99d6d18e211c97352d99e6ab
Mobb autofixer <git@mobb.ai>
2026-06-08 13:37:44 +0000

    mobb fix commit: b8c3c69c-7470-4c09-9c37-635bb8b6ccd7

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -6424,7 +6424,7 @@ THREE.Math = {
 
 	random16: function () {
 
-		return ( 65280 * Math.random() + 255 * Math.random() ) / 65535;
+		return ( 65280 * Math.random() + 255 * (crypto.getRandomValues(new Uint32Array(1))[0] / 2 ** 32) ) / 65535;
 
 	},
 
