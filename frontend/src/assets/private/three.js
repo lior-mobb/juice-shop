1650d29b807fd556241babfe9cb41674dafdaaf5
Mobb autofixer <git@mobb.ai>
2026-06-08 15:50:07 +0000

    mobb fix commit: 43f710c1-d622-40a4-b2fe-1f13b4c4862d

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -9253,7 +9253,7 @@ THREE.BufferGeometry.prototype = {
 			for ( var i = 3, il = positions.length; i < il; i += 3 ) {
 
 				var x = positions[ i ];
-				var y = positions[ i + 1 ];
+				const y = positions[ i + 1 ];
 				var z = positions[ i + 2 ];
 
 				// bounding box
