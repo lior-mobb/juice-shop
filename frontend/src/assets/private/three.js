745461f0e86e86579bf7f361559334d2e31bd337
Mobb autofixer <git@mobb.ai>
2026-06-08 13:43:36 +0000

    mobb fix commit: 5fb29898-d3ad-49fc-b8b7-03cd15173484

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -34486,7 +34486,7 @@ THREE.TorusKnotGeometry = function ( radius, tube, radialSegments, tubularSegmen
 			var uva = new THREE.Vector2( i / radialSegments, j / tubularSegments );
 			var uvb = new THREE.Vector2( ( i + 1 ) / radialSegments, j / tubularSegments );
 			var uvc = new THREE.Vector2( ( i + 1 ) / radialSegments, ( j + 1 ) / tubularSegments );
-			var uvd = new THREE.Vector2( i / radialSegments, ( j + 1 ) / tubularSegments );
+			const uvd = new THREE.Vector2( i / radialSegments, ( j + 1 ) / tubularSegments );
 
 			this.faces.push( new THREE.Face3( a, b, d ) );
 			this.faceVertexUvs[ 0 ].push( [ uva, uvb, uvd ] );
