b013054b2d300ef12facdd8ee14839dacd35aef1
Mobb autofixer <git@mobb.ai>
2026-06-08 15:46:58 +0000

    mobb fix commit: 0c8eb09c-e16f-4626-be98-bca64c8847f6

--- routes/captcha.ts
+++ routes/captcha.ts
@@ -17,7 +17,7 @@ function captchas () {
     const secondTerm = Math.floor(((crypto.getRandomValues(new Uint32Array(1))[0] / 2 ** 32) * 10) + 1)
     const thirdTerm = Math.floor((Math.random() * 10) + 1)
 
-    const firstOperator = operators[Math.floor((Math.random() * 3))]
+    const firstOperator = operators[Math.floor(((crypto.getRandomValues(new Uint32Array(1))[0] / 2 ** 32) * 3))]
     const secondOperator = operators[Math.floor((Math.random() * 3))]
 
     const expression = firstTerm.toString() + firstOperator + secondTerm.toString() + secondOperator + thirdTerm.toString()
