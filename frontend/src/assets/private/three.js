168de84cbc7390407c3672a1432532bc28da9114
Mobb autofixer <git@mobb.ai>
2026-06-08 13:42:45 +0000

    mobb fix commit: a5840c54-03af-4404-94a3-be36e6b99ea2

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -24798,7 +24798,7 @@ THREE.WebGLRenderer = function ( parameters ) {
 
 	function removeInstances( objlist, object ) {
 
-		for ( var o = objlist.length - 1; o >= 0; o -- ) {
+		for ( let o = objlist.length - 1; o >= 0; o -- ) {
 
 			if ( objlist[ o ].object === object ) {
 
