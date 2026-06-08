4e84101e64842c63749db2b5675ca88ff2eeeef9
Mobb autofixer <git@mobb.ai>
2026-06-08 13:40:16 +0000

    mobb fix commit: 011bb536-7cb6-441c-8cda-9c267591ee61

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -12692,7 +12692,7 @@ THREE.ObjectLoader.prototype = {
 
 		if ( json !== undefined ) {
 
-			var geometryLoader = new THREE.JSONLoader();
+			const geometryLoader = new THREE.JSONLoader();
 			var bufferGeometryLoader = new THREE.BufferGeometryLoader();
 
 			for ( var i = 0, l = json.length; i < l; i ++ ) {
