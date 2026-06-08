c773e257b5581224f3c4b290d3907c361bdaff3d
Mobb autofixer <git@mobb.ai>
2026-06-08 15:50:45 +0000

    mobb fix commit: e73cd728-b3ab-4ca0-b5a1-bba93ebba1f6

--- frontend/src/assets/private/OrbitControls.js
+++ frontend/src/assets/private/OrbitControls.js
@@ -236,7 +236,7 @@ THREE.OrbitControls = function ( object, domElement ) {
         // restrict phi to be betwee EPS and PI-EPS
         phi = Math.max( EPS, Math.min( Math.PI - EPS, phi ) );
 
-        var radius = offset.length() * scale;
+        let radius = offset.length() * scale;
 
         // restrict radius to be between desired limits
         radius = Math.max( this.minDistance, Math.min( this.maxDistance, radius ) );
