1ec2b79410d1c047b271b8f5a640034d1f4a406f
Mobb autofixer <git@mobb.ai>
2026-06-08 13:41:31 +0000

    mobb fix commit: 6fbb64e9-1038-4438-9002-d0cace41f1a4

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -29654,7 +29654,7 @@ THREE.CurvePath.prototype.getWrapPoints = function ( oldPts, path ) {
 		// check for out of bounds?
 
 		var pathPt = path.getPoint( xNorm );
-		var normal = path.getTangent( xNorm );
+		const normal = path.getTangent( xNorm );
 		normal.set( -normal.y, normal.x ).multiplyScalar( oldY );
 
 		p.x = pathPt.x + normal.x;
