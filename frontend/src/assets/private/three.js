0a750e2bb8ee421a51608b651523ae46c6ba279e
Mobb autofixer <git@mobb.ai>
2026-06-08 13:40:03 +0000

    mobb fix commit: 3cebdf1a-9c55-4707-81d9-aaf9be7a3038

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -36117,7 +36117,7 @@ THREE.SpotLightHelper = function ( light ) {
 	this.matrixWorld = light.matrixWorld;
 	this.matrixAutoUpdate = false;
 
-	var geometry = new THREE.CylinderGeometry( 0, 1, 1, 8, 1, true );
+	const geometry = new THREE.CylinderGeometry( 0, 1, 1, 8, 1, true );
 
 	geometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, -0.5, 0 ) );
 	geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
