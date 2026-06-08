0dcca2422a36836c5dd495df508cd22deaca7aeb
Mobb autofixer <git@mobb.ai>
2026-06-08 13:43:37 +0000

    mobb fix commit: 175157ca-b9f2-4d87-b442-ba5478d33054

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -1825,7 +1825,7 @@ THREE.Vector3.prototype = {
 
 		var x = this.x, y = this.y, z = this.z;
 
-		var e = m.elements;
+		const e = m.elements;
 
 		this.x = e[0] * x + e[4] * y + e[8]  * z;
 		this.y = e[1] * x + e[5] * y + e[9]  * z;
