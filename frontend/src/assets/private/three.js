7edc750684b6ca5d9cbbfa0f70f7e4ab7919481a
Mobb autofixer <git@mobb.ai>
2026-06-08 13:43:15 +0000

    mobb fix commit: e92119bc-6f7a-44ac-b6cd-19ad778cfd71

--- frontend/src/assets/private/three.js
+++ frontend/src/assets/private/three.js
@@ -34229,7 +34229,7 @@ THREE.SphereGeometry = function ( radius, widthSegments, heightSegments, phiStar
 
 			var n1 = this.vertices[ v1 ].clone().normalize();
 			var n2 = this.vertices[ v2 ].clone().normalize();
-			var n3 = this.vertices[ v3 ].clone().normalize();
+			const n3 = this.vertices[ v3 ].clone().normalize();
 			var n4 = this.vertices[ v4 ].clone().normalize();
 
 			var uv1 = uvs[ y ][ x + 1 ].clone();
